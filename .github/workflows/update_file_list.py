3a46d2024175c29af9ca524a5e1bd49b906e634f
MCT32 <ivanhen99@gmail.com>
2024-07-18 10:42:36 +1000

    surely first try

---
+++ .github/workflows/update_file_list.py
@@ -0,0 +1,29 @@
+import json
+import fnmatch
+
+with open('file_list.txt', 'r') as f:
+    file_list = f.read().splitlines()
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+with open('.configignore', 'r') as f:
+    ignore_list = f.read().splitlines()
+
+files = []
+config_files = []
+
+for file in file_list:
+    if fnmatch.fnmatch(file, 'config/*'):
+        config_files.append(file)
+    else:
+        for ignore in ignore_list:
+            if fnmatch.fnmatch(file, ignore):
+                break
+        else:
+            files.append(file)
+            
+config['files'] = files
+config['config_files'] = config_files
+with open('config.json', 'w') as f:
+    f.write(json.dumps(config, indent=4))
